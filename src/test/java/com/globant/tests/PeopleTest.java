6b110c5be866184cc20f27ac3dfbb8785d5166ce
Cesar <cesar.tipan@globant.com>
2023-12-29 23:10:43 -0500

    FIX: remove skinColor and filmCount parameters for the test, and create constants for each one

--- src/test/java/com/globant/tests/PeopleTest.java
+++ src/test/java/com/globant/tests/PeopleTest.java
@@ -1,18 +1,19 @@
 package com.globant.tests;
 
 import com.globant.lib.models.Person;
-import com.globant.lib.services.PeopleService;
 import com.globant.utils.tests.BaseTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import static com.globant.utils.Constants.*;
+
 public class PeopleTest extends BaseTest {
 
     @Test
-    @Parameters({"personId", "skinColor", "filmCount"})
-    public void peopleEndPointTest(int personId, String skinColor, int filmCount) {
+    @Parameters({"personId"})
+    public void peopleEndPointTest(int personId) {
         Person personResponse = getPerson(personId);
-        validateSkinColor(personResponse, skinColor);
-        validateAmountOfFilmsItAppears(personResponse, filmCount);
+        validateSkinColor(personResponse, SKIN_COLOR);
+        validateAmountOfFilmsItAppears(personResponse, FILM_COUNT);
     }
 }
