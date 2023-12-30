520e7d4e173d31ee38f84d9ac5884f4410d15351
Cesar <cesar.tipan@globant.com>
2023-12-29 19:06:09 -0500

    ADDED: SUCCESS GET STATUS CODE

--- src/test/java/com/globant/utils/Constants.java
+++ src/test/java/com/globant/utils/Constants.java
@@ -2,5 +2,5 @@ package com.globant.utils;
 
 public class Constants {
     public static final String BASE_URL = "https://swapi.dev/api/";
-
+    public static final int SUCCESS_GET_STATUS_CODE = 200;
 }
