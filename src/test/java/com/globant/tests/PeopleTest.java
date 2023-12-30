3b63399b1dff0d9a80c88025172631cfeff46809
Cesar <cesar.tipan@globant.com>
2023-12-29 19:08:33 -0500

    ADDED: Create parameters to the test and pass it to the respective methods

--- src/test/java/com/globant/tests/PeopleTest.java
+++ src/test/java/com/globant/tests/PeopleTest.java
@@ -1,14 +1,18 @@
 package com.globant.tests;
 
+import com.globant.lib.models.Person;
 import com.globant.lib.services.PeopleService;
 import com.globant.utils.tests.BaseTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class PeopleTest extends BaseTest {
 
     @Test
-    public void peopleEndPointTest() {
-        PeopleService peopleService = mainService.createPeopleService();
-
+    @Parameters({"personId", "skinColor", "filmCount"})
+    public void peopleEndPointTest(int personId, String skinColor, int filmCount) {
+        Person personResponse = getPerson(personId);
+        validateSkinColor(personResponse, skinColor);
+        validateAmountOfFilmsItAppears(personResponse, filmCount);
     }
 }
